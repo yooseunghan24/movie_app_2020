f79d4676de44c36b2191e97169af069813fff1f5
yooseunghan24 <ushan24@naver.com>
2020-08-22 19:18:20 +0900

    Updates

---
+++ precache-manifest.aa31f738ecdc233a30cb0d1ee8a61db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32e2b0ea193d86225ab2cfd44e83a6f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d7b875adde06695df419",
+    "url": "/movie_app_2020/static/css/main.bf68b71e.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b875adde06695df419",
+    "url": "/movie_app_2020/static/js/main.f248f3b0.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
